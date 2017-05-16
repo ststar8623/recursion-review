e4982f717b384a946b83cfc7f3edb02513f45a72
Hack Reactor Student <student@hackreactor.com>
2017-05-15 20:37:17 -0700

    update

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -18,7 +18,9 @@ describe('getElementsByClassName', function() {
 
       var result = getElementsByClassName('targetClassName');
       var expectedNodeList = document.getElementsByClassName('targetClassName');
+      
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+      console.log(expectedArray);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
       expect(equality).to.equal(expectedNodeList);
 
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,17 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   
-  
+  console.log(className.nodes);
 };
+
+var htmlStrings = [
+  '<div class="targetClassName"></div>',
+  '<div class="otherClassName targetClassName"></div>',
+  '<div><div class="targetClassName"></div></div>',
+  '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+  '<div><div></div><div><div class="targetClassName"></div></div></div>',
+  '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+];
+
+console.log(getElementsByClassName(htmlStrings));
